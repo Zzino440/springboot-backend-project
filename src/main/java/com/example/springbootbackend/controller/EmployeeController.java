d5b5b2cc6fade6ecd2f9cd0c020d2a270287768b
Lorenzo Belli <l.belli@dstech.it>
2024-01-14 01:36:56 +0100

    added getEmployeeById method

--- src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.springbootbackend.controller;
 
+import com.example.springbootbackend.exception.ResourceNotFoundException;
 import com.example.springbootbackend.model.Employee;
 import com.example.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,4 +28,13 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
     }
+
+    //get employee by id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + id));
+        return ResponseEntity.ok(employee);
+    }
+
 }
