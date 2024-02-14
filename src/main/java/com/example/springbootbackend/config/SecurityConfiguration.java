d3abf1623c214cc520c073ee317a69a80b2f51f3
Lorenzo Belli <l.belli@dstech.it>
2024-02-14 22:06:47 +0100

    little clean and comments

--- src/main/java/com/example/springbootbackend/config/SecurityConfiguration.java
+++ src/main/java/com/example/springbootbackend/config/SecurityConfiguration.java
@@ -32,17 +32,9 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
-
-/*                        .requestMatchers("api/v1/users/**").hasRole(ADMIN.name())
-
-                        .requestMatchers(GET,"api/v1/users/**").hasAuthority(ADMIN_READ.name())
-                        .requestMatchers(POST,"api/v1/users/**").hasAuthority(ADMIN_POST.name())
-                        .requestMatchers(PUT,"api/v1/users/**").hasAuthority(ADMIN_UPDATE.name())
-                        .requestMatchers(DELETE,"api/v1/users/**").hasAuthority(ADMIN_DELETE.name())*/
-
+                        //examples of how to use the roles on the security config
                         .requestMatchers("api/v1/demo-controller/**").hasRole(USER.name())
                         .requestMatchers(GET,"api/v1/demo-controller").hasAuthority(USER_READ.name())
-
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
