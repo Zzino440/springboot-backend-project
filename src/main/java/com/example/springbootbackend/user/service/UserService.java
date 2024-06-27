fca74c1769cfd8ef5b40f785502305b755ec8ec0
Zzino <lorenzo97belli@gmail.com>
2024-06-27 23:05:38 +0200

    added some error handling on the main functionalities (mainly the authentication and login functionalities) and added custom errors for them

--- src/main/java/com/example/springbootbackend/user/service/UserService.java
+++ src/main/java/com/example/springbootbackend/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.springbootbackend.user.service;
 
+import com.example.springbootbackend.exceptions.MyProjectError;
 import com.example.springbootbackend.exceptions.MyProjectException;
 import com.example.springbootbackend.user.DTO.UserDTO;
 import com.example.springbootbackend.user.enums.Role;
@@ -12,9 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -47,6 +50,9 @@ public class UserService {
     public Page<UserDTO> getAllUsersExceptCurrent(Long id, String email, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<User> usersPage = userRepository.findAllUsersExceptCurrentByEmailLike(id, email, pageable);
+        if (usersPage.isEmpty()) {
+            throw new MyProjectException(MyProjectError.USERS_NOT_FOUND, "No users found with the provided criteria");
+        }
         return usersPage.map(UserMapper::toUserDTO);
     }
 
@@ -100,16 +106,13 @@ public class UserService {
 
     public List<String> searchUserNamesByEmail(String email) {
         List<String> userEmails = userRepository.findUserNamesByEmailLike(email);
-/*        if (userEmails.isEmpty()) { TODO - vedere a cosa serviva questa eccezione
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Email found for the current pattern: " + email);
-        }*/
         return userEmails;
     }
 
     //utility methods
     private User findById(Long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User doesn't exist with id:" + id));
+                .orElseThrow(() -> new MyProjectException(MyProjectError.USER_NOT_FOUND, "User doesn't exist with id:" + id));
     }
 
     public void checkIfEmailExistsElsewhere(Long currentUserId, String newEmail) throws MyProjectException {
