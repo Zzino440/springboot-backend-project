5ca4af13f0b1878cb5b50accb733b23ef69fa57d
Zzino <lorenzo97belli@gmail.com>
2025-10-20 22:05:06 +0200

    refactor(CategoryDTO): clean up fields by removing commented-out code

--- src/main/java/com/example/springbootbackend/category/dto/CategoryDTO.java
+++ src/main/java/com/example/springbootbackend/category/dto/CategoryDTO.java
@@ -9,20 +9,10 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 public class CategoryDTO {
-
-    private Long id;                      // ID della categoria
-    private String name;                  // Nome della categoria
-    private String description;           // Descrizione della categoria
-    private Long vocabularyId;            // ID del vocabolario associato
-//    private String vocabularyName;        // Nome del vocabolario associato
-    private Long parentCategoryId;        // ID della categoria genitore (se esiste)
-    private String parentCategoryName;    // Nome della categoria genitore (se esiste)
-/*    private List<SubCategoryDTO> subCategories; // Lista di sottocategorie
-
-    @Data
-    @Builder
-    public static class SubCategoryDTO {
-        private Long id;                  // ID della sottocategoria
-        private String name;              // Nome della sottocategoria
-    }*/
+    private Long id;
+    private String name;
+    private String description;
+    private Long vocabularyId;
+    private Long parentCategoryId;
+    private String parentCategoryName;
 }
